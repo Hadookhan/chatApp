993590567e7e8ac1523683941163327ce768f166
Hadookhan <hadikhan05@hotmail.co.uk>
2025-09-20 13:33:40 +0100

    baseplate

---
+++ app.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+
+root = Tk()
+frm = ttk.Frame(root, padding=300)
+frm.grid()
+ttk.Label(frm, text="Chat App").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
